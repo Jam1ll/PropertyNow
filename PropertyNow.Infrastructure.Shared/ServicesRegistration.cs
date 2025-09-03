4b0fbd92422178c96786ae7777fe264fdd17571f
Jamil Guzmán <jamilguzman202@gmail.com>
2025-09-03 12:40:29 -0400

    shared layer done

---
+++ PropertyNow.Infrastructure.Shared/ServicesRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PropertyNow.Core.Application.Interfaces;
+using PropertyNow.Core.Domain.Settings;
+using PropertyNow.Infrastructure.Shared.Services;
+
+namespace PropertyNow.Infrastructure.Shared
+{
+    public static class ServicesRegistration
+    {
+        //Extension method - Decorator pattern
+        public static void AddSharedLayerIoc(this IServiceCollection services, IConfiguration config)
+        {
+            #region Configurations
+            services.Configure<MailSettings>(config.GetSection("MailSettings"));
+            #endregion
+
+            #region Services IOC
+            services.AddScoped<IEmailService, EmailService>();
+            #endregion
+        }
+    }
+}
